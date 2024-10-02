a4af4ea20101caf86964ae635f66fe6355d234b9
nazwanuramalia <nzwanur05@gmail.com>
2024-10-02 10:27:10 +0700

    Add files via upload

---
+++ soal 3 pertemuan 2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	float alas = 4.0;
+	float tinggi = 5.0;
+	float sisi_miring;
+	
+	sisi_miring = sqrt(alas * alas + tinggi * tinggi);
+	
+	printf("sisi miring segi tiga adalah: %.2f cm\n", sisi_miring);
+	
+	return 0;
+}
+
