c1cbadda5807de296fbef3c85efb946502d5c2a4
nazwanuramalia <nzwanur05@gmail.com>
2024-10-12 10:29:43 +0700

    Add files via upload

---
+++ Modul1/tugas 1 nazwa nur.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    char nim = "2441033";
+    char nama[50] = "nazwa nur amalia";
+    char jurusan[50] = "Teknik Informatika";
+
+    printf("NIM: %d\n", nim);
+    printf("Nama: %s\n", nama);
+    printf("Jurusan: %s\n", jurusan);
+
+    return 0;
+}
